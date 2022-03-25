4099181cf133a810c9c9667cfc4b97a1519ded2d
Krisi97 <kiraly.kristof6@gmail.com>
2022-03-25 20:01:39 +0100

    C# asztali kliens v1

---
+++ S-port C#/s-port/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace s_port
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Snow;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 246);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Események";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Esemenyek);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Felhasználók";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Felhasznalok);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sportok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Sportok);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(405, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Település";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Telepulesek);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(528, 424);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(270, 23);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(571, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeresesEnterre);
+            // 
+            // button5
+            // 
+            this.button5.AutoEllipsis = true;
+            this.button5.Location = new System.Drawing.Point(677, 45);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Keresés";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Kereses);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "S-port";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ S-port C#/s-port/Form1.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace s_port
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _connectionString = "datasource=localhost;port=3306;username=root;password=";
+        private string _aktualisTabla = "";
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void TablaLekeres(string tabla)
+        {
+            _aktualisTabla = tabla;           
+            string query = "SELECT * FROM sport." + tabla;
+            AdatokLekerese(query);
+        }
+        private void AdatokLekerese(string command)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command, conn))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    AdatokMegjelenitese(ds.Tables[0]);
+                }
+            }
+        }
+        private void AdatokMegjelenitese(DataTable table)
+        {
+            dataGridView1.DataSource = table;
+            label1.Text = "Találatok száma: " + table.Rows.Count;
+        }
+        private void Esemenyek(object sender, EventArgs e)
+        {
+            TablaLekeres("esemenyek");
+           
+        }
+
+        private void Felhasznalok(object sender, EventArgs e)
+        {
+            TablaLekeres("felhasznalok");
+        }
+
+        private void Sportok(object sender, EventArgs e)
+        {
+            TablaLekeres("sportok");
+        }
+
+        private void Telepulesek(object sender, EventArgs e)
+        {
+            TablaLekeres("telepulesek");
+        }
+
+        private void Kereses(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_aktualisTabla))
+            {
+                MessageBox.Show("Nem történt lekérdezés!","Hiba") ;
+            }
+            else
+            {
+                string command = "SELECT * FROM sport." + _aktualisTabla;
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command, conn))
+                    {
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+
+                        for (int i = ds.Tables[0].Rows.Count - 1; i >= 0 ; i--)
+                        {
+                            bool vanTalalat = false;                          
+                            for (int j = ds.Tables[0].Columns.Count - 1; j >= 0 ; j--)
+                            {
+                                var cellaErtek = ds.Tables[0].Rows[i][j];
+                                if (string.Equals(cellaErtek.ToString(), textBox1.Text, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    vanTalalat = true;
+                                }
+                            }
+
+                            if (!vanTalalat)
+                            {
+                                ds.Tables[0].Rows.RemoveAt(i);
+                            }
+                        }
+                        if (ds.Tables[0].Rows.Count == 0)
+                        {
+                            MessageBox.Show("Nincs találat", "Keresés");
+                        }
+                        else
+                        {
+                            AdatokMegjelenitese(ds.Tables[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void KeresesEnterre(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Kereses(null,null);
+            }
+        }
+    }
+}
