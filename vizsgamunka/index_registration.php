52671e81952312d1d03ecb3a7c2414af9a8b3a0b
Vamosatesz <vamosatesz@gmail.com>
2022-04-13 11:37:47 +0200

    .

--- vizsgamunka/index_registration.php
+++ vizsgamunka/index_registration.php
@@ -70,10 +70,10 @@ include_once 'include/connect.php';
     <div id="toggle"></div>
     <div id=navbar>
       <ul>
-        <li><a href="esemenyek.php">Események</a></li>
-        <li><a href="uj_esemeny.php">Új esemény léterhozása</a></li>
+        
         <li><a href="#">Rólunk</a></li>
-        <li><a href="#">Kijelentkezés</a></li>
+        <li><a href="#">ÁFSZ</a></li>
+        
       </ul>
     </div>
   </header>
@@ -113,8 +113,6 @@ include_once 'include/connect.php';
     }
 
     ?>
-
-
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
