3b07461f5ff0bdb0c5161e48b6bc3e9db994dfc6
Krisi97 <kiraly.kristof6@gmail.com>
2022-04-20 17:43:27 +0200

    veglesges

---
+++ vizsgamunka-vegleges/esemenyek.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<?php
+include_once 'include/connect.php';
+?>
+<html lang="HU">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/esemenyek.css">
+    <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="CSS/jquery-ui.css"-->
+    <link rel="icon" type="image/png" sizes="32x32" href="IMG/favicon2.png">
+    <script type="text/javascript" src="js/jquery-1.12.4.js" ></script>
+    <script type="text/javascript" src="js/jquery-ui.js" ></script>
+    <script type="text/javascript">
+    
+
+  $( function() {
+    $( "#from" ).datepicker();
+    } );
+
+    $( function() {
+    $( "#to" ).datepicker();
+    } );
+
+  </script>
+
+
+    <title>Események</title>
+</head>
+<body>
+
+  <!--Itt kezdődik a NAVbar-->
+
+  <header>
+    <a href="#" class="logo">S-port</a>
+    <div id="toggle"></div>
+    <div id=navbar>
+      <ul>
+        <!--li><a href="#">Események</a></li-->
+        <li><a href="../vizsgamunka/uj_esemeny.php">Új esemény léterhozása</a></li>
+        <li><a href="#">Rólunk</a></li>
+        <li><a href="#">Kijelentkezés</a></li>
+      </ul>
+    </div>
+  </header>
+  
+<!-- script kód a NAVbar-hoz-->
+  <script>
+    const header = document.getElementById('header');
+    const toggle = document.getElementById('toggle');
+    const navbar = document.getElementById('navbar');
+    
+    document.onclick = function(e){
+      if(e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar'){
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+      }
+    }
+    
+    toggle.onclick = function(){
+      toggle.classList.toggle('active');
+      navbar.classList.toggle('active');
+    }
+  </script>
+
+<!-- NAVbar vége -->
+
+<div class="container">
+<h2 style="text-align: center;font-weight:bold"> Sport események közötti keresés</h2>
+
+  <div class="row">
+  <label></label>
+    <form class="form-horizontal" action="esemenyek.php" method="POST">
+  
+      <div class="form-group">
+      <label></label>
+      <div class="col-lg-4">  
+        <select name="esemeny_hely" class="form-control" > 
+        <option>Település választása</option>
+        <?php
+        $result2=mysqli_query($connect,"select * from telepulesek");
+        while($row=mysqli_fetch_array($result2))
+        {
+         $telepules=$row['telepules_nev'];
+        echo "<option> $telepules </option>";
+        } 
+        ?>
+        </select>
+        </div>
+    </div>
+
+  <div class="form-group">
+  <label></label>
+    <div class="col-lg-4">
+      <input type="text" name="esemeny_ido" id="from" class="form-control" placeholder="Időpont">
+    </div>
+  </div>
+
+  <div class="form-group">
+  <label></label>
+    <div class="col-lg-4">
+      <input type="submit"name="submit" class="btn btn btn-outline-warning" value="Keresés">
+    </div>
+  </div>
+
+  </form>
+  </div>
+</div>
+
+
+
+      
+<div class="container">
+  <div class="table my-3">     
+    <div class="row headcolor">
+      <div class="col-sm-3 d-none d-sm-block text-white text-center fs-4 fw-bolder">Esemény név</div>
+      <div class="col-sm-3 d-none d-sm-block text-white text-center fs-4 fw-bolder">Esemény helye</div>
+      <div class="col-sm-3 d-none d-sm-block text-white text-center fs-4 fw-bolder">Esemény időpontja</div>
+      <div class="col-sm-3 d-none d-sm-block text-white text-center fs-4 fw-bolder reszletek">Részletek</div>
+    </div>
+        
+    <?php
+    include_once 'include/connect.php';
+    if(isset($_POST['submit'])) 
+    {
+        $telepulesek=$_POST['esemeny_hely'];
+        $fromdate=$_POST['esemeny_ido'];
+        $fdate= strtotime($fromdate);
+        $fdate= date("Y/m/d", $fdate);
+    
+    if ($telepulesek != "" || $fromdate != "")
+    {
+     $query= ("SELECT * FROM esemenyek INNER JOIN telepulesek ON esemenyek.telepulesek_telepules_id=telepulesek.telepules_id WHERE esemeny_ido='$fdate' AND esemenyek.telepulesek_telepules_id = telepulesek.telepules_id"); 
+     $data= mysqli_query($connect, $query) OR die('error');
+  
+      if(mysqli_num_rows($data)>0)
+      {
+          while($row=mysqli_fetch_assoc($data))
+          {
+              $esemenyneve= $row['esemeny_nev'];  
+              $telepulesek1= $row['esemeny_hely'];
+              $telepulesid= $row['telepulesek_telepules_id'];
+              $fromdate= $row['esemeny_ido'];
+              $esemenyleiras= $row['esemeny_leiras'];
+
+              $query_telepulesid= ("SELECT telepules_nev FROM telepulesek WHERE telepules_id= '$telepulesid' "); 
+              $telepid1= mysqli_query($connect, $query_telepulesid) OR die('error');
+              if(mysqli_num_rows($telepid1)>0)
+              {
+                while($row=mysqli_fetch_assoc($telepid1))
+                  {
+                    $telepid= $row['telepules_nev']; 
+                  }
+              } 
+          
+             ?>
+            <div class="row tablecolor">
+              <div class="col-lg-3 fs-5 text-center fw-bold"><?php echo $esemenyneve; ?></div>
+              <div class="col-lg-3 fs-5 text-center fw-bold"><?php echo $telepid; ?></div>
+              <div class="col-lg-3 fs-5 text-center fw-bold"><?php echo $fromdate; ?></div>
+              <div class="col-lg-3 fs-5 text-center fw-bold"><?php echo $esemenyleiras; ?></div>
+            </div> 
+            <?php 
+          } 
+      }
+      ?>
+                 <!-- else{
+                    <tr>
+                      <td>Nincs találat!</td>
+                    </tr> -->
+   <?php
+
+    }?>
+    <?php
+    }
+    ?>
+    </div> 
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script
+
+</body>
+</html>
---
+++ vizsgamunka-vegleges/index_registration.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<?php
+include_once 'include/connect.php';
+
+  if(isset($_POST['submit']))
+    {
+        $errors = array();
+        $true = true;
+        if(empty($_POST['felhasznalo_nev']))
+        {
+            $true=false;
+            array_push($errors, "A felhasználói név üres");
+        }
+        if(empty($_POST['email']))
+        {
+            $true=false;
+            array_push($errors, "Az e-mail üres");
+        }
+        if(empty($_POST['jelszo']))
+        {
+            $true=false;
+            array_push($errors, "A jelszó üres");
+        }
+        if(empty($_POST['jelszo_ujra']))
+        {
+            $true=false;
+            array_push($errors, "A jelszó megerősítése üres");
+        }
+        if(!($_POST['jelszo']==$_POST['jelszo_ujra']))
+        {
+            $true=false;
+            array_push($errors,"A jelszavak nem egyeznek meg!");
+        }
+        if($true)
+        {
+            //regisztráció
+            $felhasznalo_nev=mysqli_real_escape_string($connect, $_POST['felhasznalo_nev']);
+            $email=mysqli_real_escape_string($connect, $_POST['email']);
+            $jelszo=mysqli_real_escape_string($connect, $_POST['jelszo']);
+            $jelszo=md5($jelszo);
+
+            $sql="INSERT INTO felhasznalok(felhasznalo_nev, jelszo, email) VALUES('$felhasznalo_nev','$jelszo','$email')";
+            $connect-> query($sql);
+            session_start();
+            $_SESSION['email']=$email;
+            header('location:index_login.php');
+        }
+    }
+    $connect->close();
+?>
+<html lang="HU">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/png" sizes="32x32" href="IMG/favicon2.png">
+    <link rel="stylesheet" href="CSS/style_registration.css">
+    
+    
+    <title>Regisztráció</title>
+    
+</head>
+<body>
+
+<! --Itt kezdődik a NAVbar -->
+
+  <header>
+    <a href="#" class="logo">S-port</a>
+    <div id="toggle"></div>
+    <div id=navbar>
+      <ul>
+        <li><a href="esemenyek.php">Események</a></li>
+        <li><a href="uj_esemeny.php">Új esemény léterhozása</a></li>
+        <li><a href="#">Rólunk</a></li>
+        <li><a href="#">Kijelentkezés</a></li>
+      </ul>
+    </div>
+  </header>
+  
+
+
+
+<! -- script kód a NAVbar-hoz -->
+  <script>
+    const header = document.getElementById('header');
+    const toggle = document.getElementById('toggle');
+    const navbar = document.getElementById('navbar');
+    
+    document.onclick = function(e){
+      if(e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar'){
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+      }
+    }
+    
+    toggle.onclick = function(){
+      toggle.classList.toggle('active');
+      navbar.classList.toggle('active');
+    }
+  </script>
+
+<! -- NAVbar vége -->
+
+  
+<! -- regisztáció start -->
+
+    <form action="index_registration.php" method="POST">
+      <div class="container-login">
+          <div class="login-form">
+              <div class="login">
+                <h3>Regisztráció</h3>
+              </div>
+              <input type="text" name="felhasznalo_nev" placeholder="Felhasználónév">
+              <input type="email" name="email" placeholder="email">
+              <input type="password" name="jelszo" placeholder="jelszó">
+              <input type="password" name="jelszo_ujra" placeholder="jelszó mégegyszer">
+              <input class="btn" value="Regisztráció" type="submit" name="submit"/>
+          </div>
+      </div>    
+    </form>
+
+<! -- regisztráció finish -->
+
+    <?php
+    if(!empty($errors))
+    {
+        foreach($errors as $key)
+        {
+            echo $key."<br/>";
+        }
+    }
+
+    ?>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+
+</html>
